07e74b9d36328390bcdd710d28445478f04d76ac
ahmaruff <ahmadmaruf2701@gmail.com>
2023-02-04 15:38:54 +0700

    FEATURE: shield usermodel

---
+++ app/Models/UserModel.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use CodeIgniter\Shield\Models\UserModel as ShieldUserModel;
+
+class UserModel extends ShieldUserModel
+{
+    protected function initialize(): void
+    {
+        $this->allowedFields = [
+            ...$this->allowedFields,
+
+            // 'first_name',
+        ];
+    }
+}
