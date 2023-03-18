ee8a37503cf5165a690ba0931fc533b0d9af4235
ahmaruff <ahmadmaruf2701@gmail.com>
2023-03-18 21:26:23 +0700

    fix(app) change bootstrapp to cdn

--- app/Views/siswa/layouts/base.php
+++ app/Views/siswa/layouts/base.php
@@ -11,9 +11,13 @@
     
     <!-- CUSTOM -->
     <link rel="stylesheet" href="/assets/css/style.css">
-    
+
     <!-- BOOTSTRAP -->
+    <!-- 
     <link rel="stylesheet" href="/assets/libs/bootstrap-5.2.2/css/bootstrap.min.css">
+    -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
     <!-- BOOTSTRAP ICON -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     
@@ -68,7 +72,12 @@
 
     <!-- SB ADMIN JS -->
     <script src="/assets/libs/sbadmin-7.0.5/js/scripts.js"></script>
+    
     <!-- BOOTSTRAP -->
+    <!--
     <script src="/assets/libs/bootstrap-5.2.2/js/bootstrap.bundle.min.js"></script>
+    -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
 </body>
 </html>
